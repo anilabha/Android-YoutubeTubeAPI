6865f015cce9125d1528a78749ff45ec0f63bcbf
anilabha <anilabha91@gmail.com>
2019-05-22 19:20:03 +0530

    Initial commit

---
+++ app/src/main/java/com/echessa/videotube/Config.java
@@ -0,0 +1,14 @@
+package com.echessa.videotube;
+
+/**
+ * Created by anilabha baral*********
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAfO63OKDJmCFv4LW3_sRGhhcmVIAHoQag";
+
+}
